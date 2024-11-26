aaf7a7b7134fc1129d389349f9b5fbfe817296de
Bodmaxtech <100292307+Bodmaxtech@users.noreply.github.com>
2024-11-26 00:42:19 +0000

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom'; // Ensure this is imported
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
